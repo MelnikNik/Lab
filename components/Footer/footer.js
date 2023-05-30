f0a6bb3b023d04f9df435e79d81e24a70777a109
MelnikNik <MelnikovNI@studklg.ru>
2023-05-30 16:47:59 +0300

    comment

---
+++ components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    const divStyle = {
+        width:'100%',
+        height: '250px',
+        background: 'blue',
+        color: 'black',
+        font
+    }
+}
